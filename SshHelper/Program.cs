27981e271e1129f256a34bb75e2b49f271d94cd0
Stanislav Zavizion <StasZavi@hotmail.com>
2018-08-04 17:01:39 +0300

    first commit

---
+++ SshHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SshHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Ssh Helper!");
+        }
+    }
+}
